502cf840d6c89d1f309d816d7c488702f4648cdb
Cindy Duong <54452916+cduong21@users.noreply.github.com>
2022-02-24 23:00:04 -0800

    Files for reference
    Users should create their own environment in a parent directory

---
+++ send_sms.py
@@ -0,0 +1,20 @@
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+    .create(
+        body="What goes best with a cup of coffee? Another cup. \n \nThanks for being a loyal customer, we want to show you some love back \n \nShop our new buy 1 get 1 free promo online.",
+        from_='+19126008594',
+        # photo needs to be publically avaiable -- upload to flickr 
+        media_url=['https://live.staticflickr.com/65535/51898195883_13c197808a_b.jpg'],
+        to='+12063563088'
+    )
+
+print(message.sid)
